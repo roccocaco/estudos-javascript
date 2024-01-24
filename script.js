137c8e5a92cab6f6499e55b28d5cac245b2a986c
roccocaco <144187511+roccocaco@users.noreply.github.com>
2024-01-24 16:38:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let nome="Whitney"
+let nomes = "Madonna"
+console.log(nomes)
+console.log(nome)
---
+++ script2.js
@@ -0,0 +1,32 @@
+const idade = 15;
+const possuiCarro = 1;
+
+if(idade >= 18) {
+  console.log("O usuário pode fazer a carteira");
+}
+
+if(idade <= 17) {
+  console.log("O usuário não pode fazer a carteira");
+}
+
+if (possuiCarro) {
+  console.log("O usuário já pode andar de carro");
+}
+
+const nome = "Antenor";
+
+if(nome == "Rocco") {
+  console.log("O seu nome é Rocco");
+}
+
+if(nome != "Rocco") {
+  console.log("o meu nome não é Rocco");
+}
+
+if(20 > 10) {
+  console.log("Passou");
+}
+
+if(10 < 20) {
+  console.log("Passou 2")
+}
