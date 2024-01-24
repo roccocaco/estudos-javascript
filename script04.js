3414c064672e5f7f361ce820626b16eb0eb43bd8
roccocaco <144187511+roccocaco@users.noreply.github.com>
2024-01-24 16:38:54 -0300

    Add files via upload

---
+++ script04.js
@@ -0,0 +1,9 @@
+const grade = 65;
+
+if (grade >= 80) {
+  console.log("Parabéns, voce faz parte do grupo de pessoas aprovadas !");
+ } else if (grade < 80 && grade >= 60) {
+  console.log("Voce está na nossa lista de espera.");
+ } else {
+  console.log("Infelizmente, voce reprovou.");
+ }
---
+++ script3.js
@@ -0,0 +1,11 @@
+let n1 = 2;
+let n2 = 1;
+
+
+if (n1 > n2) {
+  console.log("n1 é maior que n2");
+} else if(n1 === n2) {
+  console.log("eles são iguais")
+} else {
+  console.log("n2 é maior que n1")
+}
