a783cbd5d457c29ae6cdb0ca1f93d03d1198d69e
roccocaco <144187511+roccocaco@users.noreply.github.com>
2024-01-25 20:18:55 -0300

    Add files via upload

---
+++ script09.js
@@ -0,0 +1,21 @@
+const instrumentos = ["guitarra", "bateria", "Violao", "teclado"];
+
+const instPrecos = [1200.0, 3000.0, 500.0, 515.0];
+
+console.log(instrumentos[3], instPrecos[3]);
+
+const qtds = [3, 5, 12, 3]
+
+const resultado = qtds[0] * instPrecos[0]
+console.log(resultado)
+
+const guitarra = ["guitarra"];
+
+const bateria = ["bateria"]
+
+const violao = ["violao"]
+
+const teclado = ["teclado"]
+
+
+
