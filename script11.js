683ba95354a7dbabc037c80749704f2d7a6be327
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-01-27 18:47:07 -0300

    Add files via upload

---
+++ script11.js
@@ -0,0 +1,9 @@
+const instrumentos = ["guitarra", "bateria", "Violao", "teclado"];
+
+const instPrecos = [1200.0, 3000.0, 500.0, 515.0];
+
+const qtds = [3, 5, 12, 3]
+
+let valorFinal = 0
+valorFinal = qtds[0] * instPrecos [0]
+console.log(valorFinal)
