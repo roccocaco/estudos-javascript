71a55f224d35c8c5dab58b99e0a98de9f4981292
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-01-30 20:43:13 -0300

    Add files via upload

---
+++ script13.js
@@ -0,0 +1,7 @@
+const exames = ["sangue", "urina", "fezes", "colesterol"];
+
+const preco = [50, 20, 30, 75]
+
+let resultado = preco [2] + preco [3]
+console.log(`O preco de um exame de fezes e colesterol é: R$ ${resultado},00`)
+
