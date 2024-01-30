f830a03dfbdab62a51a71e2b062e9dcac32ee183
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-01-29 21:22:46 -0300

    Add files via upload

---
+++ script12.js
@@ -0,0 +1,15 @@
+const pizzas = ["frango", "queijo", "portuguesa", "americana"]
+
+const tamanho = ["pequena", "media", "grande", "gigante"]
+
+const precoFrango = [20, 30, 40, 50]
+
+const precoQueijo = [24, 34, 44, 54]
+
+const precoPortuguesa = [25, 35, 45, 55]
+
+const precoAmericana = [30, 40, 50, 60]
+
+let precoFinal = 0
+precoFinal = precoFrango[0] + tamanho[2] * 2
+console.log(precoFinal)
