d981c341e31163c3af2af71c060c51ae6583ba5f
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-01 15:36:36 -0300

    Add files via upload

---
+++ script14.js
@@ -0,0 +1,11 @@
+const exames = [
+  {exame: `urina`, preco: 50},
+  {exame: `fezes`, preco: 55},
+  {exame: `sangue`, preco: 75},
+  {exame: `colesterol`, preco: 80},
+];
+
+console.log(`O preco do exame de ${exames[0].exame} custa ${exames[0].preco} reais`)
+console.log(`O preco do exame de ${exames[1].exame} custa ${exames[1].preco} reais`)
+console.log(`O preco do exame de ${exames[2].exame} custa ${exames[2].preco} reais`)
+console.log(`O preco do exame de ${exames[3].exame} custa ${exames[3].preco} reais`)
---
+++ script15.js
@@ -0,0 +1,21 @@
+const search = [
+  {nome: `alegrifes e rabujos`},
+  {ano: 2003-2004},
+  {tipo: `novela`},
+  {trailer: `https://www.youtube.com/watch?v=errZjxWabQs`},
+
+  {nome: `serafim`},
+  {ano: 2001},
+  {tipo: `novela`},
+  {trailer: `https://www.youtube.com/watch?v=DtAA37HXrWU`},
+
+  {nome: `carrossel`},
+  {ano: 2012},
+  {tipo: `novela`},
+  {trailer: `https://www.youtube.com/watch?v=aSCm9JTUEIQ`},
+
+  {nome: `Amy`},
+  {ano: 2004},
+  {tipo: `novela`},
+  {trailer: `https://www.youtube.com/watch?v=esMVxSxfTAE`},
+]
