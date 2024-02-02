3ba2ec4b39763a4ac91b532730344ba6560ee685
Rocco <144187511+roccocaco@users.noreply.github.com>
2024-02-02 15:16:46 -0300

    Add files via upload

---
+++ array02.js
@@ -0,0 +1,15 @@
+const doce = ["bolo", "brigadeiro", "beijinho", "bala"]
+
+const salgado = ["coxinha", "kibe", "risolis", "bolinho"]
+
+const bebida = ["refrigerante", "suco", "cerveja", "agua"]
+
+const precoDoce = [4, 3, 2, 1]
+
+const precoSalgado = [1, 2 , 3, 4]
+
+const precoBebida = [2, 1, 4, 3]
+
+let final = 0
+final = precoBebida[0] + precoSalgado[2] + precoDoce [1]  
+console.log(final)
