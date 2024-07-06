2cc3b14e911da5b162b488747f8b73d1f2e27725
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:00:55 -0300

    update

--- script10.js
+++ script10.js
@@ -1,25 +1,12 @@
-let products = [{
-  name: 'caneta',
-  value: 2.30,
-  quantity: 3
-},
-{
-  name: 'caderno',
-  value: 13.40,
-  quantity: 2
-},
-{
-  name: 'borracha',
-  value: 4.2,
-  quantity: 5
-}
-]
+let products = [
+  { name: "caneta", value: 2.3, quantity: 3 },
+  { name: "caderno", value: 13.4, quantity: 2 },
+  { name: "borracha", value: 4.2, quantity: 5 },
+];
 
-let total = products.reduce(calculate, 0, 1, 2,3);
+// Função de redução para calcular o total
+let total = products.reduce((acc, product) => {
+  return acc + product.value * product.quantity;
+}, 0);
 
-function calculate (total, products) {
-return total + (item.value * item.quantity);
-}
-
-
-
+console.log(total); // Saída: 56.40
