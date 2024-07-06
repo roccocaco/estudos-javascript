8f4033227c5c452c896f9722372210decea8f5f2
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:05:19 -0300

    update

--- script13.js
+++ script13.js
@@ -1,7 +1,9 @@
+// Arrays de tipos de exames e preços correspondentes
 const exames = ["sangue", "urina", "fezes", "colesterol"];
+const preco = [50, 20, 30, 75];
 
-const preco = [50, 20, 30, 75]
+// Cálculo do preço total para exame de fezes e colesterol
+let resultado = preco[2] + preco[3];
 
-let resultado = preco [2] + preco [3]
-console.log(`O preco de um exame de fezes e colesterol é: R$ ${resultado},00`)
-
+// Exibição do resultado formatado no console
+console.log(`O preço de um exame de fezes e colesterol é: R$ ${resultado},00`);
