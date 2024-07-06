02f459035578a9ba32f57e4d9cb1f562fa9a5e7c
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 21:54:04 -0300

    update

--- script07.js
+++ script07.js
@@ -1,16 +1,16 @@
-const lanche = "X-tudo"
+const lanche = "X-tudo"; // Declaração da variável 'lanche' e atribuição do valor "X-tudo"
 
 switch (lanche.toLocaleLowerCase()) {
   case "x-tudo":
-    console.log("R$12,00")  
+    console.log("R$12,00"); // Se 'lanche' for "x-tudo", imprime o preço no console
     break;
   case "x-bacon":
-    console.log("R$10,00") 
-    break; 
+    console.log("R$10,00"); // Se 'lanche' for "x-bacon", imprime o preço no console
+    break;
   case "x-salada":
-    console.log("R$8,00")
+    console.log("R$8,00"); // Se 'lanche' for "x-salada", imprime o preço no console
     break;
   default:
-    console.log("Não esxite nenhum lanche !")
+    console.log("Não existe nenhum lanche!"); // Se 'lanche' for qualquer outro valor, imprime mensagem de erro no console
     break;
 }
