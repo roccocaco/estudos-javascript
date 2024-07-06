b75d98424dd131baedadce94bb53744052c403d9
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 22:08:06 -0300

    update

--- script15.js
+++ script15.js
@@ -1,21 +1,22 @@
+// Array contendo informações sobre diferentes programas de televisão
 const search = [
-  {nome: `alegrifes e rabujos`},
-  {ano: 2003-2004},
-  {tipo: `novela`},
-  {trailer: `https://www.youtube.com/watch?v=errZjxWabQs`},
+  { nome: `alegrifes e rabujos` },
+  { ano: 2003 - 2004 }, // Calcula o intervalo de anos (2003 a 2004)
+  { tipo: `novela` },
+  { trailer: `https://www.youtube.com/watch?v=errZjxWabQs` },
 
-  {nome: `serafim`},
-  {ano: 2001},
-  {tipo: `novela`},
-  {trailer: `https://www.youtube.com/watch?v=DtAA37HXrWU`},
+  { nome: `serafim` },
+  { ano: 2001 },
+  { tipo: `novela` },
+  { trailer: `https://www.youtube.com/watch?v=DtAA37HXrWU` },
 
-  {nome: `carrossel`},
-  {ano: 2012},
-  {tipo: `novela`},
-  {trailer: `https://www.youtube.com/watch?v=aSCm9JTUEIQ`},
+  { nome: `carrossel` },
+  { ano: 2012 },
+  { tipo: `novela` },
+  { trailer: `https://www.youtube.com/watch?v=aSCm9JTUEIQ` },
 
-  {nome: `Amy`},
-  {ano: 2004},
-  {tipo: `novela`},
-  {trailer: `https://www.youtube.com/watch?v=esMVxSxfTAE`},
-]
+  { nome: `Amy` },
+  { ano: 2004 },
+  { tipo: `novela` },
+  { trailer: `https://www.youtube.com/watch?v=esMVxSxfTAE` },
+];
