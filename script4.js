2e2e98851895cba1f313682b672083e922fd9bc9
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 21:49:07 -0300

    update

--- script4.js
+++ script4.js
@@ -1,13 +1,23 @@
-let idade = 75
+// Declarando uma variável 'idade' e atribuindo o valor 75 a ela
+let idade = 75;
 
-if (idade < 2 ) {
-  console.log("é um bebe")
+// Verificando se a idade é menor que 2
+if (idade < 2) {
+  // Se a condição for verdadeira, imprime "é um bebê" no console
+  console.log("é um bebe");
 } else if (idade < 10) {
-  console.log("é uma crianca")
+  // Se a condição anterior for falsa, verifica se a idade é menor que 10
+  // Se esta condição for verdadeira, imprime "é uma criança" no console
+  console.log("é uma crianca");
 } else if (idade < 18) {
-  console.log("é um adolescente")
+  // Se as condições anteriores forem falsas, verifica se a idade é menor que 18
+  // Se esta condição for verdadeira, imprime "é um adolescente" no console
+  console.log("é um adolescente");
 } else if (idade < 60) {
-  console.log("é um adulto")
+  // Se as condições anteriores forem falsas, verifica se a idade é menor que 60
+  // Se esta condição for verdadeira, imprime "é um adulto" no console
+  console.log("é um adulto");
 } else {
-  console.log("é um idoso")
+  // Se todas as condições anteriores forem falsas, imprime "é um idoso" no console
+  console.log("é um idoso");
 }
