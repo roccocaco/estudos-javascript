56f7c8d067d6d7f0a326818a2c7f563f7592b2ab
Rocco Caco <32roccocaco@gmail.com>
2024-07-05 21:42:26 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let nome="Whitney"
-let nomes = "Madonna"
-console.log(nomes)
-console.log(nome)
+// Declarando uma variável 'primeiroNome' e atribuindo a string "Whitney" a ela
+let primeiroNome = "Whitney";
+
+// Declarando uma variável 'nomeArtista' e atribuindo a string "Madonna" a ela
+let nomeArtista = "Madonna";
+
+// Imprimindo o valor da variável 'nomeArtista' no console
+console.log(nomeArtista); // Saída: Madonna
+
+// Imprimindo o valor da variável 'primeiroNome' no console
+console.log(primeiroNome); // Saída: Whitney
